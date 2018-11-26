b99b598c579ddf7ae3024cb1517e4a143a1b5c1e
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-26 11:49:34 +0000

    completed lab but tests not passing/working

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React from 'react'
+import Greeting from './components/Greeting'
+import ExampleComponent from './components/ExampleComponent'
+import Button from './components/Button'
+
+
+class App extends React.Component {
+
+    state = {
+        spacePeople: []
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    async getData() {
+        const response = await fetch('http://api.open-notify.org/astros.json')
+        const spacePeople = await response.json()
+        this.setState({ spacePeople })
+    }
+
+    render() {
+        return (
+            <div>
+                <Greeting />
+                <ExampleComponent />
+                <Button />
+            </div>
+        )}
+
+}
+
+export default App 
